e92dbff9f54393f30e45650f7dd8fe5960187a79
Guido <Guido@HPzBook15G2>
2020-04-08 23:12:08 +0200

    Upload

---
+++ src/beispiele/ForSchleife.java
@@ -0,0 +1,36 @@
+package beispiele;
+
+/***************************************************************/
+/* Das Programm zeigt die Arbeitsweise der for-Schleife        */
+/* anhand einer int-Laufvariablen.                             */
+/***************************************************************/
+
+
+
+public class ForSchleife {
+
+ /**
+ * @param args
+ */
+public static void main(String args[]) {
+
+   int i; /* Laufvariable */
+
+   System.out.println();
+
+   
+   /* for-Schleife
+      - initialisiert zu Beginn i mit dem Wert 1
+      - wird (erstmalig oder erneut) durchlaufen, solange i <= 10 ist
+      - zaehlt nach jedem Durchlauf i um 1 hoch */
+
+   for (i=1; i<=15; i++) {
+
+    System.out.print("i, i hoch 2, i hoch 3: ");
+    System.out.println(i + " " + i*i + " " + i*i*i);
+    
+
+   }
+
+ }
+}
