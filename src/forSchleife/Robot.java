a820e2ae8dad56ebc966dbf3b735930f007d351f
Guido <Guido@HPzBook15G2>
2020-04-09 14:56:22 +0200

    Go

---
+++ src/forSchleife/Robot.java
@@ -0,0 +1,21 @@
+package forSchleife;
+
+class Robot {
+
+    void countTo(int number){
+    	
+    	 int i; /* Laufvariable */
+
+    	   System.out.println();
+
+    	   
+    	   /* for-Schleife
+    	      - initialisiert zu Beginn i mit dem Wert 1
+    	      - wird (erstmalig oder erneut) durchlaufen, solange i <= 10 ist
+    	      - zaehlt nach jedem Durchlauf i um 1 hoch */
+
+    	   for (i=1; i<=20; i++) {
+    		   System.out.println(i);
+    }
+
+    }}
